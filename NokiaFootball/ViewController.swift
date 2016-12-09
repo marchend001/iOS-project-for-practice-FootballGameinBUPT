d7f4a487e4954ed62e2de1f4d85240a4f5066635
龚茜 <gongqian@gongqiandemac.local>
2016-12-09 19:36:39 +0800

    Initial Commit

---
+++ NokiaFootball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NokiaFootball
+//
+//  Created by 龚茜 on 16/12/9.
+//  Copyright © 2016年 龚茜. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
